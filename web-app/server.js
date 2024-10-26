36654d25c29d49c0fc7b7c6488aa5bdeb11e2558
marcus-mcmullenjr <marcus.mcmullenjr@gmail.com>
2024-10-25 23:56:11 -0400

    new update

--- web-app/server.js
+++ web-app/server.js
@@ -1,3 +1,4 @@
+import ServerlessHttp from 'serverless-http';
 const express = require('express');
 const path = require('path');
 
@@ -8,10 +9,15 @@ const port = 3100;
 app.use(express.static(__dirname));
 
 // Serve the HTML file
-app.get('/', (req, res) => {
+app.get('/.netlify/web-app/server', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+const handler = ServerlessHttp(app);
+mudule.exports.handler = async(event, context) => {
+  const result
+}
